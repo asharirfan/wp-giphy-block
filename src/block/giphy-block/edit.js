4e38580ad02f1a3fc8264be6c45e1b0a7c51f7b3
Ashar Irfan <mrasharirfan@gmail.com>
2020-09-17 02:41:54 +0500

    👌 IMPROVE: Gifs search and select functionality

--- src/block/giphy-block/edit.js
+++ src/block/giphy-block/edit.js
@@ -1,6 +1,8 @@
-import { RichText } from '@wordpress/block-editor';
+import { Button, Card, CardBody, TextControl, Spinner } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import './editor.scss';
+import axios from 'axios';
+
+// import './editor.scss';
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -15,24 +17,95 @@ import './editor.scss';
 export default function Edit( props ) {
 	const {
 		attributes: {
-			content,
+			searchActive,
+			searchResults,
+			selectedGif,
 		},
 		className,
 		setAttributes,
 	} = props;
 
-	// Update field content on change.
-	const onChangeContent = ( newContent ) => {
-		setAttributes( { content: newContent } );
+	const searchGiphy = ( query ) => {
+		if ( ! query ) {
+			return;
+		}
+
+		setAttributes( { searchActive: true, searchResults: [] } );
+
+		try {
+			const fetch = axios( {
+				url: `https://api.giphy.com/v1/gifs/search?api_key=cpiNEMWpw4D3DXaT4QIu7A8RZCnupclo&q=${ query }&limit=25&offset=0&rating=r&lang=en`,
+				method: 'GET',
+				credentials: 'same-origin',
+			} );
+			fetch.then( ( response ) => {
+				setAttributes( { searchActive: false } );
+
+				if ( 200 !== response.status ) {
+					return;
+				}
+
+				const results = response.data.data.reduce( ( gifs, currentGif ) => {
+					gifs.push( {
+						url: currentGif.images.original.url,
+						title: currentGif.title,
+					} );
+					return gifs;
+				}, [] );
+
+				setAttributes( { searchResults: results } );
+			} );
+		} catch ( error ) {
+			console.log( error ); // eslint-disable-line no-console
+		}
+	};
+
+	const setSelectedGif = ( gif ) => {
+		const selected = [];
+		selected.url = gif.url;
+		selected.title = gif.title;
+		setAttributes( { selectedGif: selected, searchResults: [] } );
 	};
 
 	return (
-		<RichText
-			tagName="p"
-			className={ className }
-			onChange={ onChangeContent }
-			value={ content }
-			placeholder={ __( 'AsharIrfan Giphy Block Demo', 'giphy-block' ) }
-		/>
+		<div className={ className }>
+			<Card className="giphy-block-search">
+				<CardBody>
+					<TextControl
+						label={ __( 'Giphy Block', 'giphy-block' ) }
+						placeholder={ __( 'Search for GIFs here...', 'giphy-block' ) }
+						onChange={ searchGiphy }
+					/>
+					{ searchActive && ( <Spinner /> ) }
+				</CardBody>
+			</Card>
+			{ 0 !== searchResults.length ? (
+				<Card className="giphy-block-search-results">
+					{ searchResults.map( ( gif, index ) => {
+						return (
+							<div key={ index }>
+								<Button
+									onClick={ () => setSelectedGif( gif ) }
+									style={ {
+										display: 'block',
+										width: '100%',
+										height: '100%',
+									} }
+								>
+									<img src={ gif.url } alt={ gif.title } />
+								</Button>
+							</div>
+						);
+					} ) }
+				</Card>
+			) : false }
+			{ 0 !== selectedGif.length ? (
+				<Card>
+					<CardBody>
+						<img src={ selectedGif.url } alt={ selectedGif.title } />
+					</CardBody>
+				</Card>
+			) : false }
+		</div>
 	);
 }
